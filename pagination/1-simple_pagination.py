807299343163dfeb5ad97ea1a01cdc9b8fcb6dea
amina <amina.1111@yahoo.fr>
2024-05-03 11:33:29 +0100

    Simple pagination

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -5,17 +5,6 @@ import math
 from typing import List
 
 
-def index_range(page, page_size):
-    """The function should return a tuple of size
-    two containing a start index and
-    an end index corresponding to the range of indexes to
-    return in a list for those particular pagination parameters.
-    """
-    start_index = (page - 1) * page_size
-    end_index = start_index + page_size
-    return (start_index, end_index)
-
-
 class Server:
     """Server class to paginate a database of popular baby names."""
 
@@ -34,13 +23,23 @@ class Server:
 
         return self.__dataset
 
+    def index_range(page, page_size):
+        """The function should return a tuple of size
+        two containing a start index and
+        an end index corresponding to the range of indexes to
+        return in a list for those particular pagination parameters.
+        """
+        start_index = (page - 1) * page_size
+        end_index = start_index + page_size
+        return (start_index, end_index)
+
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """Simple pagination"""
         assert (
-            type(page) is int
-            and type(page_size) is int
+            isinstance(page, int)
+            and isinstance(page_size, int)
             and page > 0
             and page_size > 0
         )
-        start_index, end_index = index_range(page, page_size)
+        start_index, end_index = self.index_range(page, page_size)
         return self.dataset()[start_index:end_index]
