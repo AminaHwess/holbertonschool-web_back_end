224400b320de1daeabf350a398eed6b6138ed22d
amina <amina.1111@yahoo.fr>
2024-05-07 13:35:44 +0100

    12-log_stats.py

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Script that provides some stats about Nginx logs using MongoDB"""
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+    """ provides some stats about Nginx logs stored in MongoDB """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    collection = client.logs.nginx
+
+    print(f"{collection.count_documents()} logs")
+
+    print("Methods:")
+    for x in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        method_count = collection.count_documents({'method': x})
+        print(f"\tmethod {x}: {method_count}")
+
+    check_get = collection.count_documents(
+        {'method': 'GET', 'path': "/status"})
+    print(f"{check_get} status check")
