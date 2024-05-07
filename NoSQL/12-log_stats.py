b017a8761fac235a7c43d2ffc318a1e067d7706e
amina <amina.1111@yahoo.fr>
2024-05-07 13:46:01 +0100

    12-log_stats.py

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -8,13 +8,13 @@ if __name__ == "__main__":
     collection = client.logs.nginx
 
     total_logs = collection.count_documents({})
-    print(f"{total_logs} logs")
+    print(f'{total_logs} logs')
 
-    print("Methods:")
+    print('Methods:')
     for x in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
-        method_count = collection.count_documents({'method': x})
-        print(f"\tmethod {x}: {method_count}")
+        method_count = collection.count_documents({"method": x})
+        print(f'\tmethod {x}: {method_count}')
 
     check_get = collection.count_documents(
-        {'method': 'GET', 'path': "/status"})
+        {"method": "GET", "path": "/status"})
     print(f"{check_get} status check")
