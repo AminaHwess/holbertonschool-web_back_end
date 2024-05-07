8935e3c6d65e1931159b51f721ffd635a99af5c4
amina <amina.1111@yahoo.fr>
2024-05-07 13:47:20 +0100

    12-log_stats.py

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python3
-"""Script that provides some stats about Nginx logs using MongoDB"""
+"""script that provides some stats about Nginx logs stored in MongoDB:"""
+
 from pymongo import MongoClient
 
 if __name__ == "__main__":
-    """ provides some stats about Nginx logs stored in MongoDB """
+    """ provides some stats about Nginx logs stored in MongoDB"""
     client = MongoClient('mongodb://127.0.0.1:27017')
     collection = client.logs.nginx
 
-    total_logs = collection.count_documents({})
-    print(f'{total_logs} logs')
-
+    logs = collection.count_documents({})
+    print(f'{logs} logs')
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     print('Methods:')
-    for x in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
-        method_count = collection.count_documents({"method": x})
-        print(f'\tmethod {x}: {method_count}')
+    for x in methods:
+        count = collection.count_documents({"method": x})
+        print(f'\tmethod {x}: {count}')
 
-    check_get = collection.count_documents(
-        {"method": "GET", "path": "/status"})
-    print(f"{check_get} status check")
+    status_check = collection.count_documents(
+        {"method": "GET", "path": "/status"}
+        )
+    print(f'{status_check} status check')
