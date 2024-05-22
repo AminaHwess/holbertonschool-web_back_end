8fdf5a55c0cfb29661513f4337de0ff52c706dd0
amina <amina.1111@yahoo.fr>
2024-05-22 16:32:00 +0100

    Handle multiple successful promises

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,12 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise
-    .all([uploadPhoto(), createUser()])
+  Promise.all([uploadPhoto(), createUser()])
     .then((value) => {
       console.log(value[0].body, value[1].firstName, value[1].lastName);
     })
     .catch(() => {
-      console.log('Signup system offline');
+      console.error('Signup system offline');
     });
 }
