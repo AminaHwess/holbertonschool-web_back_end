bc36233e7f46a704adcad38cd0019878f3d984f1
amina <amina.1111@yahoo.fr>
2024-05-22 16:15:37 +0100

    Handle multiple successful promises

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -5,7 +5,7 @@ export default function handleProfileSignup() {
     .then((value) => {
       console.log(value[0].body, value[1].firstName, value[1].lastName);
     })
-    .catch(() => {
+    .catch((error) => {
       console.log('Signup system offline');
     });
 }
