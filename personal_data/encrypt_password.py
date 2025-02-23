6e5432f00833bed89ee7db2aaf76fc68513b5a28
amina <amina.1111@yahoo.fr>
2025-02-23 12:49:25 +0100

    personal_data

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -12,7 +12,7 @@ def hash_password(password: str) -> bytes:
     hash = bcrypt.hashpw(bytes, salt)
     return hash
 
-
+ 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """is_valid function that the provided password
     matches the hashed password"""
